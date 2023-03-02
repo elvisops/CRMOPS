48e1d1bb70fd3836b7e551da27c12949ffc47176
asorianoops <alberto.soriano@ops.com.hn>
2023-03-02 10:29:00 -0600

    Inicializacion de proyecto

---
+++ src/app/modules/administracion/administracion-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdministracionComponent } from './administracion.component';
+
+const routes: Routes = [
+    {path:'',component:AdministracionComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AdministracionRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production:true,
+    api:"http//10.8.8.81:1900/",
+    crypto_key:"crm@&0ps)#"
+
+};
