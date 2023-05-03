54df303a105bb9afc8220b9cb64c5e7a4fc6b83d
asorianoops <alberto.soriano@ops.com.hn>
2023-05-03 16:15:13 -0600

    updating

--- src/app/modules/administracion/socios/proyectos/carteras/carteras-crear/carteras-crear.component.ts
+++ src/app/modules/administracion/socios/proyectos/carteras/carteras-crear/carteras-crear.component.ts
@@ -33,6 +33,7 @@ export class CarterasCrearComponent implements OnInit {
   ListaCarterasTipos:CarterasTipos[] = []
   StepOneCompleted:boolean = false
   CarteraID:number = 0
+  Bitacora:string = "Proceso de creacion de cartera:"
 
   //Step 2
   LoadingFile:boolean = false  
@@ -42,6 +43,7 @@ export class CarterasCrearComponent implements OnInit {
   SecondStepCompleted:boolean = false    
 
   EncabezadoTablaDetalles:string[] = []
+
 
   //Arreglos sin duplicados para crear registros en la base de datos
   ArrIdentidadesNombres:string[] = []
@@ -86,26 +88,28 @@ export class CarterasCrearComponent implements OnInit {
 
   //==========================================================================================================//
   CrearCartera(){
+    this.Bitacora += `\n1. Se crea la cartera con los siguientes datos: \nNombre: ${this.firstFormGroup.get('NombreCartera')?.value} \nTipo: ${this.firstFormGroup.get('TipoCartera')?.value}`
     var NombreCartera = this.firstFormGroup.get('NombreCartera')?.value
     var TipoCarteraID = this.firstFormGroup.get('TipoCartera')?.value
-    var ProyectoID = this.Proyecto    
-    console.log(ProyectoID)
+    var ProyectoID = this.Proyecto       
 
     this.service.Create(ProyectoID,TipoCarteraID,NombreCartera).subscribe(r=>{
       var respuesta = this.auth.desencriptar(r.response)
       respuesta = JSON.parse(respuesta)
       respuesta = respuesta[0]
+      this.Bitacora += `\n2. Conectando con la base de datos...`
       
       if(respuesta.status == 1){
         this.StepOneCompleted = true
         this.CarteraID = respuesta.data
         this.service.notificacion("Cartera registrada con exito")
-
+        this.Bitacora += `\n3. Cartera registrada con exito`
         this.firstFormGroup.controls['NombreCartera'].disable()
         this.firstFormGroup.controls['TipoCartera'].disable()        
 
       }else{
         this.service.notificacion(respuesta.message)
+        this.Bitacora += `\n3. ${respuesta.message}`
         this.StepOneCompleted = false
         return;
       }
@@ -116,44 +120,52 @@ export class CarterasCrearComponent implements OnInit {
 
   //==========================================================================================================//
   convertirExcelToJson(){
+    this.Bitacora += `\n4. Leyendo archivo de excel...`
     this.LoadingFile = true
     const archivo = this.archivoInput.nativeElement.files[0];
     const reader = new FileReader();
     reader.onload = (event) => {
-      
+      this.Bitacora += `\n5. Cargando datos en memoria...`
       const data = event.target?.result;
       const workbook = XLSX.read(data, { type: 'binary' });
       const sheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[sheetName];
       this.jsonData = XLSX.utils.sheet_to_json(worksheet);
       //console.log(this.jsonData); 
+      
       this.getEncabezados(this.jsonData);
       //console.log(this.Encabezados)
       this.LoadingFile = false
       console.log(this.jsonData)
     };
     reader.readAsBinaryString(archivo);
+    this.Bitacora += `\n6. Datos cargados...`
+    this.Bitacora += `\n7. Registros Totales: ${this.jsonData.length}`
     
   };
   //==========================================================================================================//
 
   //==========================================================================================================//
   getEncabezados(Obj:any){
+    this.Bitacora += `\n7. Obteniendo nombre de columnas...`
     var arrTemp = []    
     for(var obj1 of Obj){
       const objKeys = Object.keys(obj1);
       arrTemp.push(...objKeys)
     }
     this.Encabezados = [...new Set(arrTemp)]
+    this.Bitacora += `\n8. Columnas obtenidas...`
   }
 
   getEncTablaDetalles(Obj:any){
+    this.Bitacora += `\n7. Obteniendo nombre de columnas para tabla de detalles...`
     var arrTemp = []    
     for(var obj1 of Obj){
       const objKeys = Object.keys(obj1);
       arrTemp.push(...objKeys)
     }
     this.EncabezadoTablaDetalles = [...new Set(arrTemp)]
+    this.Bitacora += `\n8. Columnas obtenidas...`
   }
 
   RemoveEnc(enc:string){
@@ -170,13 +182,15 @@ export class CarterasCrearComponent implements OnInit {
   async GenerarArreglosCarga(){
 
     this.CargandoCuentas = true
-
+    this.Bitacora += `\n9. Generando carga de datos para base de datos...`
     if(this.EncCuenta == "" || this.EncIdentidad == "" || this.EncNombre == ""){
       this.service.notificacion("Los campos obligatorios no pueden estar vacios")
+      this.Bitacora += `\n...Los campos obligatorios no pueden estar vacios, reintente la carga`
       return
     }
 
     var paquete:any = []
+    
     this.jsonData.map((cuenta:any)=>{
       paquete.push({
         CARTERAID:this.CarteraID,
@@ -186,17 +200,20 @@ export class CarterasCrearComponent implements OnInit {
       })
     })
      this.service.SendDataCuenta(paquete).subscribe(async (r:any)=>{
+      this.Bitacora += `\n10. Enviando datos a base de datos...`
       var respuesta = this.auth.desencriptar(r.response)
       respuesta = JSON.parse(respuesta)
       respuesta = respuesta[0]
       if(respuesta.status == 1){
         console.log(respuesta)          
         this.service.notificacion("Cuentas registradas con exito")
+        this.Bitacora += `\n11. Cuentas registradas con exito`
         
         this.CargandoCuentas = false        
         var cuentas_ids = JSON.parse(respuesta.data)
         console.log(cuentas_ids)
         await this.CrearTablaDetalles(cuentas_ids)
+        this.Bitacora += `\n12. Generando carga de datos para tabla de detalles...`
         this.getEncTablaDetalles(this.jsonData)
         console.log(JSON.stringify(this.EncabezadoTablaDetalles))
 
@@ -216,7 +233,6 @@ export class CarterasCrearComponent implements OnInit {
     for(var i = 0; i < this.jsonData.length; i++){
       this.jsonData[i].CUENTAIDDB = datos.filter((cuenta:any)=> cuenta.CUENTA == this.jsonData[i][this.EncCuenta])[0].CUENTAID
     }
-
     console.log(this.jsonData)
   }
 
