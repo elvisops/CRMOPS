3907ba61b74bbeaa56ea66e29c5ef5c3f37ac63b
Elvis Godoy <elvis.godoy@OPS.Local>
2023-05-10 09:33:33 -0600

    Perfil de Usuario, creacion y actualizacion

---
+++ src/app/modules/administracion/perfil-usuario/perfil-usuario.ts
@@ -0,0 +1,9 @@
+export interface PerfilUsuario {
+    USUARIOID:number,
+    USUARIO:string,
+    ROLID:number,
+    ESTADO:boolean,
+    INTENTOS:number,
+    CREACION:string,
+    ACTUALIZACION:string
+}
